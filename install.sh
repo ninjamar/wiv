1339588f77548795845ac75154ba0d93ddde3091
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-06-10 15:36:16 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
-//a
+#this file installs viw and all it's dependencies
+#this file currently only works on linux
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+if [[ $machine -eq "Linux"]] 
+then
+  sudo apt update
+  sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0 gir1.2-webkit2-4.0
+  pip3 install pycairo
+  pip3 install pygobject
+  git clone https://github.com/ninjamar/viw
+  sudo echo 'alias viw=~"python3 /viw/viw/viw.py"' >> ~/.bashrc
+  sudo source ~/.bashrc
