6a56a0e101a07a26b94f7dd018079142c5676883
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-06-14 19:21:56 -0600

    Validate urls + add google search

--- wiv/wiv.py
+++ wiv/wiv.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-import gi
-gi.require_version('Gtk','3.0')
-gi.require_version('WebKit2','4.0')
-from gi.repository import Gtk,WebKit2
+import requests
 class Window(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self,title="Wiv")
@@ -22,8 +18,17 @@ class Window(Gtk.Window):
         self.head.set_custom_title(self.search_bar)
         self.connect('check-resize',self.reposition)
     def enter_page(self,entry):
-        url = entry.get_text()
-        self.webview.load_uri(url)
+        data = entry.get_text()
+        p_url = urlparse(data)
+        if p_url.scheme in ('https','http'):
+            try:
+                requests.get(f'{p_url.scheme}://{p_url.netloc}')
+                self.webview.load_uri(data)
+            except requests.exceptions.ConnectionError:
+                self.webview.load_uri('https://ninjamar.github.io/wiv/invalid-page.html')
+        else:
+             self.webview.load_uri(f'https://google.com/search?q={data}')
+        #self.webview.load_uri(url)
     def reposition(self,window):
         self.search_bar.set_size_request(self.get_size()[0]/1.3,30)
 
